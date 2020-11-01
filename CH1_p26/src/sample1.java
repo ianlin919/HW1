74c8e7e820c70664c07dee743410c692eb8df062
ianlin0919 <ian880919@gmail.com>
2020-11-01 15:07:09 +0800

    first

---
+++ CH1_p26/src/sample1.java
@@ -0,0 +1,16 @@
+
+public class sample1 {
+	public static void main(String[] args)
+	{
+		int num,num2;
+		
+		num= 3;
+		
+		System.out.println("變數num1的值是:"+num);
+		
+		num = 5;
+		
+		System.out.println("更新變數num的值");
+		System.out.println("變數num更新後的值是:"+num);
+	}
+}
